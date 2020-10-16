e6de979b0bd1c50ad957c11023d1aba43cb921f4
Anuradha Sinha <a.sinha@reply.com>
2020-10-16 23:54:02 +0100

    SAPCC-5: Modified for alignment in backoffice

--- ext-cloudinary/cloudinary/cloudinarymediabackoffice/backoffice/src/uk/ptr/cloudinary/services/CloudinaryConfigAdminUsageRenderer.java
+++ ext-cloudinary/cloudinary/cloudinarymediabackoffice/backoffice/src/uk/ptr/cloudinary/services/CloudinaryConfigAdminUsageRenderer.java
@@ -35,6 +35,105 @@ public class CloudinaryConfigAdminUsageRenderer extends AbstractEditorAreaCompon
     public CloudinaryConfigAdminUsageRenderer() {
     }
 
+    @Override
+    public void render(Component component, AbstractSection abstractSectionConfiguration, CloudinaryConfigModel cloudinaryConfigModel, DataType dataType, WidgetInstanceManager widgetInstanceManager) {
+
+        Div usageResponseDiv = new Div();
+        final Div enableCloudinaryRadioDiv = new Div();
+        final Div radioDiv = new Div();
+        final Html nextHtml = new Html();
+        final Html html = new Html();
+        Hbox boxHeader = new Hbox();
+
+        Label enableCloudinaryFieldName = new Label(CloudinarymediacoreConstants.ENABLE_CLOUDINARY);
+        Label cloudinaryConnectionLabel = new Label();
+        Label connectionErrorMessage = new Label("");
+
+        Radio trueCheck = new Radio();
+        trueCheck.setLabel("True");
+
+        Radio falseCheck = new Radio();
+        falseCheck.setLabel("False");
+
+        setStyleAndContent(component, cloudinaryConfigModel, usageResponseDiv, enableCloudinaryRadioDiv, radioDiv, nextHtml, enableCloudinaryFieldName, cloudinaryConnectionLabel, connectionErrorMessage, trueCheck, falseCheck);
+
+        if (cloudinaryConfigModel.getEnableCloudinary()) {
+            try {
+                setConnectionDetailsOnDiv(cloudinaryConfigModel, usageResponseDiv, cloudinaryConnectionLabel, html, boxHeader);
+            } catch (IllegalArgumentException illegalException) {
+                LOG.error("Illegal Argument " + illegalException.getMessage());
+            } catch (Exception e) {
+                LOG.error("Exception occured calling Admin Usage API " + e.getMessage());
+            }
+        } else {
+            cloudinaryConnectionLabel.setValue(CloudinarymediacoreConstants.NOT_CONNECTED);
+            boxHeader.appendChild(cloudinaryConnectionLabel);
+        }
+
+        trueCheck.addEventListener(Events.ON_CLICK, (event) -> {
+            modelService.refresh(cloudinaryConfigModel);
+            String response = setConnectionDetailsOnDiv(cloudinaryConfigModel, usageResponseDiv, cloudinaryConnectionLabel, html, boxHeader);
+            if (response.equalsIgnoreCase("true")) {
+                falseCheck.setChecked(Boolean.FALSE);
+                cloudinaryConfigModel.setEnableCloudinary(true);
+                connectionErrorMessage.setValue("");
+                modelService.save(cloudinaryConfigModel);
+            } else {
+                falseCheck.setChecked(Boolean.TRUE);
+                trueCheck.setChecked(Boolean.FALSE);
+                cloudinaryConnectionLabel.setValue(CloudinarymediacoreConstants.NOT_CONNECTED);
+                html.setContent("&nbsp;");
+                boxHeader.appendChild(cloudinaryConnectionLabel);
+                connectionErrorMessage.setValue(response);
+            }
+        });
+
+        falseCheck.addEventListener(Events.ON_CLICK, (event) -> {
+            modelService.refresh(cloudinaryConfigModel);
+            trueCheck.setChecked(Boolean.FALSE);
+            cloudinaryConnectionLabel.setValue(CloudinarymediacoreConstants.NOT_CONNECTED);
+            html.setContent("");
+            boxHeader.appendChild(cloudinaryConnectionLabel);
+            connectionErrorMessage.setValue("");
+
+            cloudinaryConfigModel.setEnableCloudinary(false);
+            modelService.save(cloudinaryConfigModel);
+        });
+
+        usageResponseDiv.appendChild(boxHeader);
+        usageResponseDiv.appendChild(html);
+        usageResponseDiv.appendChild(connectionErrorMessage);
+        usageResponseDiv.setParent(component);
+
+    }
+
+    private void setStyleAndContent(Component component, CloudinaryConfigModel cloudinaryConfigModel, Div usageResponseDiv, Div enableCloudinaryRadioDiv, Div radioDiv, Html nextHtml, Label enableCloudinaryFieldName, Label cloudinaryConnectionLabel, Label connectionErrorMessage, Radio trueCheck, Radio falseCheck) {
+        nextHtml.setContent("&nbsp;<br>");
+
+        UITools.modifySClass(enableCloudinaryFieldName, "yw-enablelabelstyle", true);
+        UITools.modifySClass(cloudinaryConnectionLabel, "yw-labelstyle-z-label", true);
+        UITools.modifySClass(connectionErrorMessage, "yw-error-connection-labelstyle-z-label", true);
+
+        enableCloudinaryRadioDiv.appendChild(enableCloudinaryFieldName);
+
+        if (cloudinaryConfigModel.getEnableCloudinary()) {
+            trueCheck.setChecked(Boolean.TRUE);
+            falseCheck.setChecked(Boolean.FALSE);
+        } else {
+            trueCheck.setChecked(Boolean.FALSE);
+            falseCheck.setChecked(Boolean.TRUE);
+        }
+
+        trueCheck.setParent(component);
+        falseCheck.setParent(component);
+
+        radioDiv.appendChild(trueCheck);
+        radioDiv.appendChild(falseCheck);
+        usageResponseDiv.appendChild(enableCloudinaryRadioDiv);
+        usageResponseDiv.appendChild(radioDiv);
+        usageResponseDiv.appendChild(nextHtml);
+    }
+
     protected String setConnectionDetailsOnDiv(CloudinaryConfigModel cloudinaryConfigModel, Div newValueContainer, Label label, Html html, Hbox boxHeader) throws Exception {
 
         ApiResponse response = null;
@@ -77,101 +176,6 @@ public class CloudinaryConfigAdminUsageRenderer extends AbstractEditorAreaCompon
         newValueContainer.appendChild(boxHeader);
         newValueContainer.appendChild(html);
         return Boolean.TRUE.toString();
-    }
-
-    @Override
-    public void render(Component component, AbstractSection abstractSectionConfiguration, CloudinaryConfigModel cloudinaryConfigModel, DataType dataType, WidgetInstanceManager widgetInstanceManager) {
-
-        Div usageResponseDiv = new Div();
-        final Div enableCloudinaryRadioDiv = new Div();
-        final Div radioDiv = new Div();
-        final Html nextHtml = new Html();
-        final Html html = new Html();
-        Hbox boxHeader = new Hbox();
-
-        nextHtml.setContent("&nbsp;<br>");
-
-        Label enableCloudinaryFieldName = new Label(CloudinarymediacoreConstants.ENABLE_CLOUDINARY);
-        Label cloudinaryConnectionLabel = new Label();
-        Label connectionErrorMessage = new Label("");
-
-        UITools.modifySClass(enableCloudinaryFieldName, "yw-enablelabelstyle", true);
-        UITools.modifySClass(cloudinaryConnectionLabel, "yw-labelstyle-z-label", true);
-        UITools.modifySClass(connectionErrorMessage, "yw-error-connection-labelstyle-z-label", true);
-
-        enableCloudinaryRadioDiv.appendChild(enableCloudinaryFieldName);
-
-        Radio trueCheck = new Radio();
-        trueCheck.setLabel("True");
-
-
-        Radio falseCheck = new Radio();
-        falseCheck.setLabel("False");
-
-        if (cloudinaryConfigModel.getEnableCloudinary()) {
-            trueCheck.setChecked(Boolean.TRUE);
-            falseCheck.setChecked(Boolean.FALSE);
-        } else {
-            trueCheck.setChecked(Boolean.FALSE);
-            falseCheck.setChecked(Boolean.TRUE);
-        }
-
-        trueCheck.setParent(component);
-        falseCheck.setParent(component);
-
-        radioDiv.appendChild(trueCheck);
-        radioDiv.appendChild(falseCheck);
-        usageResponseDiv.appendChild(enableCloudinaryRadioDiv);
-        usageResponseDiv.appendChild(radioDiv);
-        usageResponseDiv.appendChild(nextHtml);
-
-        if (cloudinaryConfigModel.getEnableCloudinary()) {
-            try {
-                setConnectionDetailsOnDiv(cloudinaryConfigModel, usageResponseDiv, cloudinaryConnectionLabel, html, boxHeader);
-            } catch (IllegalArgumentException illegalException) {
-                LOG.error("Illegal Argument " + illegalException.getMessage());
-            } catch (Exception e) {
-                LOG.error("Exception occured calling Admin Usage API " + e.getMessage());
-            }
-        } else {
-            cloudinaryConnectionLabel.setValue(CloudinarymediacoreConstants.NOT_CONNECTED);
-        }
-
-        trueCheck.addEventListener(Events.ON_CLICK, (event) -> {
-            modelService.refresh(cloudinaryConfigModel);
-            String response = setConnectionDetailsOnDiv(cloudinaryConfigModel, usageResponseDiv, cloudinaryConnectionLabel, html, boxHeader);
-            if (response.equalsIgnoreCase("true")) {
-                falseCheck.setChecked(Boolean.FALSE);
-                cloudinaryConfigModel.setEnableCloudinary(true);
-                connectionErrorMessage.setValue("");
-                modelService.save(cloudinaryConfigModel);
-            } else {
-                falseCheck.setChecked(Boolean.TRUE);
-                trueCheck.setChecked(Boolean.FALSE);
-                cloudinaryConnectionLabel.setValue(CloudinarymediacoreConstants.NOT_CONNECTED);
-                connectionErrorMessage.setValue(response);
-                html.setContent("&nbsp;");
-                boxHeader.appendChild(cloudinaryConnectionLabel);
-            }
-        });
-
-        falseCheck.addEventListener(Events.ON_CLICK, (event) -> {
-            modelService.refresh(cloudinaryConfigModel);
-            trueCheck.setChecked(Boolean.FALSE);
-            cloudinaryConnectionLabel.setValue(CloudinarymediacoreConstants.NOT_CONNECTED);
-            html.setContent("");
-            boxHeader.appendChild(cloudinaryConnectionLabel);
-            connectionErrorMessage.setValue("");
-
-            cloudinaryConfigModel.setEnableCloudinary(false);
-            modelService.save(cloudinaryConfigModel);
-        });
-
-        usageResponseDiv.appendChild(boxHeader);
-        usageResponseDiv.appendChild(html);
-        usageResponseDiv.appendChild(connectionErrorMessage);
-        usageResponseDiv.setParent(component);
-
     }
 
 }
