bc7413744f105b88c573c0f110a51f4c6aaa3825
Anuradha Sinha <a.sinha@reply.com>
2020-11-25 10:10:39 +0000

    SAPCC-26: Modified for productGallery Widget

---
+++ ext-cloudinary/cloudinary/cloudinarymediaaddon/acceleratoraddon/web/webroot/WEB-INF/views/responsive/cms/productgallerywidgetcomponent.jsp
@@ -0,0 +1,6 @@
+
+<%@ page trimDirectiveWhitespaces="true"%>
+<%@ taglib prefix="product" tagdir="/WEB-INF/tags/responsive/product" %>
+
+<script src="https://product-gallery.cloudinary.com/all.js"></script>
+<div id="my-gallery" style="max-width:80%;margin:auto"/>
---
+++ ext-cloudinary/cloudinary/cloudinarymediacore/src/uk/ptr/cloudinary/service/impl/DefaultUpdateTagApiService.java
@@ -0,0 +1,36 @@
+package uk.ptr.cloudinary.service.impl;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import de.hybris.platform.core.model.media.MediaModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.ptr.cloudinary.service.UpdateTagApiService;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+
+public class DefaultUpdateTagApiService implements UpdateTagApiService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DefaultUpdateTagApiService.class);
+
+    @Override
+    public void updateTagOnAsests(String publicId, String productCode, String cloudinaryURL) throws IOException {
+        try {
+            Cloudinary cloudinary = new Cloudinary(cloudinaryURL);
+
+            String[] publicIds = {publicId};
+
+            Map result = cloudinary.uploader().addTag(productCode, publicIds, ObjectUtils.emptyMap());
+        }
+        catch (IllegalArgumentException illegalException) {
+            LOG.error("Illegal Argument " + illegalException.getMessage(), illegalException);
+        }
+        catch (Exception e) {
+            LOG.error("Exception occurred calling Upload  API " + e.getMessage() , e);
+        }
+    }
+
+}
