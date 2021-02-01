1ca9669e88312b2ad1b6c12b4d3de4542dd6b02e
Raghavendra Venugopal <r.venugopal@reply.com>
2021-02-01 20:49:13 +0000

    Added separate B2b config

--- ext-cloudinary/cloudinary/cloudinarymediafacades/src/uk/ptr/cloudinary/facades/populator/DefaultCloudinaryImagePopulator.java
+++ ext-cloudinary/cloudinary/cloudinarymediafacades/src/uk/ptr/cloudinary/facades/populator/DefaultCloudinaryImagePopulator.java
@@ -96,14 +96,26 @@ public class DefaultCloudinaryImagePopulator extends ImagePopulator implements P
                     transformationURL.append(source.getCloudinaryType());
                     transformationURL.append(CloudinarymediacoreConstants.SLASH);
 
-                    if (org.apache.commons.lang.StringUtils.isNotBlank(source.getCloudinaryTransformation())) {
-                        transformationURL.append(source.getCloudinaryTransformation());
-                    }
-
-                    if (org.apache.commons.lang.StringUtils.isNotBlank(source.getCloudinaryTransformation())) {
+                    if(BooleanUtils.isTrue(cloudinaryConfigModel.getCloudinaryResponsive())) {
+                        transformationURL.append("w_auto");
+                        transformationURL.append(",");
+                    }else {
+                        if (org.apache.commons.lang.StringUtils.isNotBlank(source.getCloudinaryTransformation())) {
+                            transformationURL.append(source.getCloudinaryTransformation());
                             transformationURL.append(",");
                         }
-                    transformationURL.append(cloudinaryConfigModel.getCloudinaryQuality());
+                    }
+
+                    if (CloudinarymediacoreConstants.IMAGE.equalsIgnoreCase(source.getCloudinaryResourceType())) {
+                        transformationURL.append("q_"+String.valueOf(cloudinaryConfigModel.getCloudinaryQuality()).toLowerCase());
+                        transformationURL.append(",");
+                        transformationURL.append("f_" +String.valueOf(cloudinaryConfigModel.getCloudinaryImageFormat()).toLowerCase());
+                    }
+                    else if(CloudinarymediacoreConstants.VIDEO.equalsIgnoreCase(source.getCloudinaryResourceType())){
+                        transformationURL.append("q_" + String.valueOf(cloudinaryConfigModel.getCloudinaryVideoQuality()).toLowerCase());
+                        transformationURL.append(",");
+                        transformationURL.append("f_" + String.valueOf(cloudinaryConfigModel.getCloudinaryVideoFormat()).toLowerCase());
+                    }
 
                     transformationURL.append(CloudinarymediacoreConstants.SLASH);
                     transformationURL.append(source.getCloudinaryVersion());
